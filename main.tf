2fc9cd8e500187d8119637ce02750dab89aac4e1
Taylor Ramsey <WRamsey@avidxchange.com>
2021-02-18 00:41:11 -0500

    Terraform Main
    pushing main.tf terraform file for azure stuff

---
+++ main.tf
@@ -0,0 +1,44 @@
+##############################################################################################################################
+# Provider Block
+##############################################################################################################################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+##############################################################################################################################
+# Resource Block
+##############################################################################################################################
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "metroplexstorage001"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "metroplexcontainer001"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
